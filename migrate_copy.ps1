cbf958361a68db7d3919dce3878ec91e62ae3f3a
Sudhakar Jinuka <sjinuka@anergroup.com>
2025-05-23 17:08:27 +0530

    Added file to repo

--- migrate_copy.ps1
+++ migrate_copy.ps1
@@ -7,10 +7,17 @@ param (
     [string]$TargetPath = "/home/sjinuka/"
 )
 
-# Compose SSH command to run SCP from source to destination
-$scpCommand = "scp -o StrictHostKeyChecking=no $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
-$sshCommand = "ssh ${SourceUser}@${SourceHost} '$scpCommand'"
+# # Compose SSH command to run SCP from source to destination
+# $scpCommand = "scp -o StrictHostKeyChecking=no $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
+# $sshCommand = "ssh ${SourceUser}@${SourceHost} '$scpCommand'"
 
-# Execute from Jenkins
+# # Execute from Jenkins
+# Write-Host "Executing remote SCP via SSH..."
+# Invoke-Expression $sshCommand
+
+# Disable host key checking and ignore known_hosts file
+$scpCommand = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
+$sshCommand = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SourceUser}@${SourceHost} '$scpCommand'"
+
 Write-Host "Executing remote SCP via SSH..."
 Invoke-Expression $sshCommand
