d8ee85b06741fc0fcbeca7222f8864e4ac136b05
Sudhakar Jinuka <sjinuka@anergroup.com>
2025-05-23 17:48:56 +0530

    Added file to repo

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,14 +2,17 @@ pipeline {
     agent any
 
     environment {
-        GIT_REPO = 'https://github.com/SudhakarJinuka/test_case4.git'
-        BRANCH = 'main'
+        GIT_REPO    = 'https://github.com/SudhakarJinuka/test_case4.git'
+        BRANCH      = 'main'
+
         SOURCE_USER = 'sjinuka'
         SOURCE_HOST = '10.128.0.29'
-        DEST_USER = 'sjinuka'
-        DEST_HOST = '10.128.0.28'
-        DEST_PATH = '/home/sjinuka/'
-        FILE_PATH = '/home/sjinuka/sample_data1.csv'
+
+        DEST_USER   = 'sjinuka'
+        DEST_HOST   = '10.128.0.28'
+        DEST_PATH   = '/home/sjinuka/'
+
+        FILE_NAME   = '/home/sjinuka/sample_data2.csv'
     }
 
     stages {
@@ -22,7 +25,15 @@ pipeline {
         stage('Transfer CSV File from Source VM to Destination VM') {
             steps {
                 sh '''
-                pwsh -Command "& { ./migrate_copy.ps1 -SourceUser \\"${SOURCE_USER}\\" -SourceHost \\"${SOURCE_HOST}\\" -DestinationUser \\"${DEST_USER}\\" -DestinationHost \\"${DEST_HOST}\\" -CsvFilePath \\"${FILE_NAME}\\" -TargetPath \\"${DEST_PATH}\\" }"
+                pwsh -Command "& {
+                    ./migrate_copy.ps1 `
+                        -SourceUser \\"${SOURCE_USER}\\" `
+                        -SourceHost \\"${SOURCE_HOST}\\" `
+                        -DestinationUser \\"${DEST_USER}\\" `
+                        -DestinationHost \\"${DEST_HOST}\\" `
+                        -CsvFilePath \\"${FILE_NAME}\\" `
+                        -TargetPath \\"${DEST_PATH}\\"
+                }"
                 '''
             }
         }
--- migrate_copy.ps1
+++ migrate_copy.ps1
@@ -1,23 +1,21 @@
 param (
     [string]$SourceUser = "sjinuka",
     [string]$SourceHost = "10.128.0.29",
-    [string]$DestinationUser = "sjinuka", 
-    [string]$DestinationHost = "10.128.0.28", 
-    [string]$CsvFilePath = "/home/sjinuka/Sample_data1.csv",
+    [string]$DestinationUser = "sjinuka",
+    [string]$DestinationHost = "10.128.0.28",
+    [string]$CsvFilePath = "/home/sjinuka/sample_data2.csv",
     [string]$TargetPath = "/home/sjinuka/"
 )
 
-# # Compose SSH command to run SCP from source to destination
-# $scpCommand = "scp -o StrictHostKeyChecking=no $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
-# $sshCommand = "ssh ${SourceUser}@${SourceHost} '$scpCommand'"
-
-# # Execute from Jenkins
-# Write-Host "Executing remote SCP via SSH..."
-# Invoke-Expression $sshCommand
-
-# Disable host key checking and ignore known_hosts file
-$scpCommand = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
-$sshCommand = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SourceUser}@${SourceHost} '$scpCommand'"
+# Specify identity file path
+$IdentityFile = "~/.ssh/id_rsa"  # Or the correct key path
 
 Write-Host "Executing remote SCP via SSH..."
+
+# scp from source to destination
+$scpCommand = "scp -i $IdentityFile -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $CsvFilePath ${DestinationUser}@${DestinationHost}:$TargetPath"
+
+# SSH into source and execute SCP
+$sshCommand = "ssh -i $IdentityFile -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SourceUser}@${SourceHost} '$scpCommand'"
+
 Invoke-Expression $sshCommand
